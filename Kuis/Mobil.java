df0aa0d2e120eb5370af4d21f82feaf247050a82
Najwaalya <najwaalya999@gmail.com>
2024-03-08 17:22:02 +0700

    new

--- Kuis/Mobil.java
+++ Kuis/Mobil.java
@@ -12,64 +12,107 @@ public class Mobil {
         this.thnProduksi = thnProduksi;
     }
     
-    double hitungPajak(double pajak){
+    double hitungPajak(){
         if (cc > 1500){
-            pajak = 10/100 * hargaBeli;
+            return 0.1 * hargaBeli;
         } else {
-            pajak = (0.0075 * hargaBeli);
+            return 0.075 * hargaBeli;
         }
-        return pajak;
     }
 
-    double hitungBiayaPerawatan(double byPerawatan){
+    double hitungBiayaPerawatan(){
         if (thnProduksi < 2010) {
-            byPerawatan = 8/100 * hargaBeli;
+            return 0.08 * hargaBeli;
         } else {
-            byPerawatan = 5/100 * hargaBeli;
+            return 0.05 * hargaBeli;
         } 
-        return byPerawatan;
     } 
 
-    double hitungBiayaTotal(double pajak, double byPerawatan){
-        return hitungPajak(pajak) + hitungBiayaPerawatan(byPerawatan);
+    double hitungBiayaTotal(){
+        return hitungPajak() + hitungBiayaPerawatan();
     }
 
-    void printData() {
-        double pajak = hitungPajak(cc);
-        double byPerawatan = hitungBiayaPerawatan(pajak);
-        double totalBiaya = hitungBiayaTotal(pajak, byPerawatan);
-        System.out.println("MobilL " + nama + " dengan biaya total sebesar " + totalBiaya + " dengan rincian: ");
+    double hitungRataBiayaPerawatan (Mobil[] mobil){
+        double totalBiayaPerawatan = 0;
+        int hitungMobil = 0;
+
+        for(Mobil m : mobil) {
+            if (m.thnProduksi > 2010) {
+                totalBiayaPerawatan += m.hitungBiayaPerawatan();
+                hitungMobil++;
+            }
+        }
+
+        if (hitungMobil > 0) {
+            return totalBiayaPerawatan / hitungMobil;
+        } else {
+            return 0;
+        }
+    }
+
+    Mobil biayaTotalTerbesar (Mobil[] mobil) {
+        Mobil mobilTerbesar = null;
+        double biayaTotalTerbesar = 0;
+
+        for (Mobil m : mobil) {
+            if (m.cc > 1500 && m.thnProduksi < 2010) {
+                double biayaTotal = m.hitungBiayaTotal();
+                if (biayaTotal > biayaTotalTerbesar) {
+                    biayaTotalTerbesar = biayaTotal;
+                    mobilTerbesar = m;
+                }
+            }
+        }
+
+        return mobilTerbesar; 
+    }
+    
+    void printData(Mobil[] mobil) {
+        double pajak = hitungPajak();
+        double byPerawatan = hitungBiayaPerawatan();
+        double totalBiaya = hitungBiayaTotal();
+        double rataBiayaPerawatan = hitungRataBiayaPerawatan(mobil);
+        Mobil totalTerbesar = biayaTotalTerbesar(mobil);
+        System.out.println("Mobil " + nama + " dengan biaya total sebesar " + totalBiaya + " dengan rincian: ");
         System.out.println("- Pajak: " + pajak);
         System.out.println("- Biaya Perawatan: " + byPerawatan);
-    }
+        System.out.println("- Rata-rata biaya perawatannya adalah " + rataBiayaPerawatan);
+        if (totalTerbesar != null) {
+            System.out.println("Biaya total terbesar mobil adalah " + totalTerbesar.hitungBiayaTotal());
+        } else {
+            System.out.println("Tidak ada mobil yang memenuhi kriteria untuk biaya total terbesar.");
+        }
+    }    
 
     public static void main(String[] args) {
         Scanner sc = new Scanner (System.in);
 
-            System.out.println("Masukkan banyak mobil yang akan dihitung: ");
+            System.out.print("Masukkan banyak mobil yang akan dihitung: ");
             int jmlMobil = sc.nextInt();
             sc.nextLine();
 
             Mobil[] daftarMobil = new Mobil[jmlMobil];
             
             for (int i = 0; i < jmlMobil; i++) {
-                System.out.println("Masukkan data mobil");
-                System.out.println("Masukkan nama mobil ke-" + (i+1));
+                System.out.println("Masukkan data mobil: ");
+                System.out.print("Masukkan nama mobil ke-" + (i+1) + ": ");
                 String nama = sc.nextLine();
-                System.out.println("Masukkan harga beli mobil ke- " + (i+1));
+                System.out.print("Masukkan harga beli mobil ke- " + (i+1) + ": ");
                 int hargaBeli =sc.nextInt();
-                System.out.println("Masukkan cc dari mobil ke: " + (i+1));
+                System.out.print("Masukkan cc dari mobil ke: " + (i+1) + ": ");
                 int cc = sc.nextInt();
-                System.out.println("Masukkan tahun produksi mobil ke- " + (i+1));
+                System.out.print("Masukkan tahun produksi mobil ke- " + (i+1) + ": ");
                 int thnProduksi = sc.nextInt();
 
-
                 daftarMobil[i] = new Mobil(nama, cc, hargaBeli, thnProduksi);
+                sc.nextLine();
             }
 
             for (Mobil x : daftarMobil){
-                x.printData();
+                x.printData(daftarMobil);
             }   
+
+            
     }
 }
     
